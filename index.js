b12c847fe84d32bb0dbfe3685a435c769313b5c5
erapoorvgit <156533574+erapoorvgit@users.noreply.github.com>
2024-01-13 22:44:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ io.on('connection', (socket) => {
     socket.on('login', (userData) => {
         const { username, secretCode } = userData;
 
-        if (secretCode === 'apoorvSecretCode' && !users['Apoorv']) {
-            users['Apoorv'] = socket.id;
-            socket.join('Apoorv-room');
-            socket.emit('loginSuccess', 'Apoorv');
-        } else if (secretCode === 'asthaSecretCode' && !users['Astha']) {
-            users['Astha'] = socket.id;
-            socket.join('Astha-room');
-            socket.emit('loginSuccess', 'Astha');
+        if (!users[username]) {
+            users[username] = [socket.id];
+            socket.join(`${username}-room`);
+            socket.emit('loginSuccess', username);
+        } else if (!users[username].includes(socket.id)) {
+            users[username].push(socket.id);
+            socket.join(`${username}-room`);
+            socket.emit('loginSuccess', username);
         } else {
             socket.emit('loginFailure', 'Invalid credentials or user already logged in.');
         }
@@ -34,10 +34,10 @@ io.on('connection', (socket) => {
 
     socket.on('message', (data) => {
         const { from, to, message } = data;
-        const targetRoom = `${from}-room`; // Form a room name based on the sender
+        const targetRoom = `${to}-room`; // Form a room name based on the recipient
 
-        if (to && users[to]) {
-            io.to(users[to]).emit('message', { from, message });
+        if (to && users[to] && users[to].includes(socket.id)) {
+            io.to(targetRoom).emit('message', { from, message });
         } else {
             socket.emit('messageFailure', 'Invalid recipient or user not found.');
         }
@@ -45,10 +45,15 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         // Handle user disconnect and cleanup
-        const username = Object.keys(users).find(key => users[key] === socket.id);
+        const username = Object.keys(users).find(key => users[key].includes(socket.id));
+
         if (username) {
-            delete users[username];
-            console.log(`${username} disconnected`);
+            users[username] = users[username].filter(id => id !== socket.id);
+
+            if (users[username].length === 0) {
+                delete users[username];
+                console.log(`${username} disconnected`);
+            }
         }
     });
 });
