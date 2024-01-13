452f8c324e8c3d964fef2c14dbf1edfb5d8209c5
erapoorvgit <156533574+erapoorvgit@users.noreply.github.com>
2024-01-13 22:28:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-const express = require('express')
-const socketIo = require('socket.io')
-const http = require('http')
-const PORT = process.env.PORT || 8001
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server,{ 
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 8001;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
     cors: {
-      origin: '*'
+        origin: '*'
     }
-}) //in case server and client run on different urls
-io.on('connection',(socket)=>{
-  console.log('client connected: ',socket.id)
-  
-  socket.join('clock-room')
-  
-  socket.on('disconnect',(reason)=>{
-    console.log(reason)
-  })
-})
-setInterval(()=>{
-     io.to('clock-room').emit('time', new Date())
-},1000)
-server.listen(PORT, err=> {
-  if(err) console.log(err)
-  console.log('Server running on Port ', PORT)
-})
+});
+
+// User data
+const users = {};
+
+io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+
+    socket.on('login', (userData) => {
+        const { username, secretCode } = userData;
+
+        if (secretCode === 'apoorvSecretCode' && !users['Apoorv']) {
+            users['Apoorv'] = socket.id;
+            socket.join('Apoorv-room');
+            socket.emit('loginSuccess', 'Apoorv');
+        } else if (secretCode === 'asthaSecretCode' && !users['Astha']) {
+            users['Astha'] = socket.id;
+            socket.join('Astha-room');
+            socket.emit('loginSuccess', 'Astha');
+        } else {
+            socket.emit('loginFailure', 'Invalid credentials or user already logged in.');
+        }
+    });
+
+    socket.on('message', (data) => {
+        const { from, to, message } = data;
+        const targetRoom = `${from}-room`; // Form a room name based on the sender
+
+        if (to && users[to]) {
+            io.to(users[to]).emit('message', { from, message });
+        } else {
+            socket.emit('messageFailure', 'Invalid recipient or user not found.');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        // Handle user disconnect and cleanup
+        const username = Object.keys(users).find(key => users[key] === socket.id);
+        if (username) {
+            delete users[username];
+            console.log(`${username} disconnected`);
+        }
+    });
+});
+
+server.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log('Server running on Port ', PORT);
+});
